6f28b9aa4c85d50331f72cd2f7e889e96e63051a
Pradip Magadum <pradipmagadum@gmail.com>
2026-01-25 13:59:24 +0530

    Initial commit

---
+++ Calculator.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOPsMiniProject
+{
+    internal class Calculator
+    {
+        public int Add(int a, int b)
+        {
+            return a + b;
+        }
+
+        public int Subtract(int a, int b)
+        {
+            return a - b;
+        }
+
+        // You can add Multiply and Divide methods too
+    }
+}
