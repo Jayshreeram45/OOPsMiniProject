e8e8bec71c104ec6697098c14f085da42750bc47
Pradip Magadum <pradipmagadum@gmail.com>
2026-01-25 14:27:23 +0530

    Added new C# program

---
+++ BankAccount.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOPsMiniProject
+{
+    internal class BankAccount
+    {
+        long AccountNo;
+        String HolderName = "";
+        Decimal Balance;
+
+        internal void Deposit(int Amount)
+        {
+            Balance += Amount;
+            Console.WriteLine("Amount Deposited in the bank");
+        }
+        internal void Withdraw(int Amount)
+        {
+            Balance -= Amount;
+            if (Balance < 500)
+                Console.WriteLine("Balance Never Be Below 500");
+            else
+            {
+                Console.WriteLine("After Withdraw Amount From Bank remaining Balance in my bank Account");
+                Console.WriteLine(Balance);
+            }
+        }
+
+    }
+}
