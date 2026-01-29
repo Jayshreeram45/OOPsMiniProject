0633d5aa329a8a1131e6567429864fea80a42212
Pradip Magadum <pradipmagadum@gmail.com>
2026-01-29 22:06:01 +0530

    Initial Comment New Program Compound Interest.

---
+++ CI.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOPsMiniProject
+{
+    internal class CI
+    {
+        Double Amount, Principal,CompoundInterest;
+        int Interest, Time;
+        internal CI(double Principal, int Interest, int Time)
+        {
+            this.Principal = Principal;
+            this.Interest = Interest;
+            this.Time = Time;
+        }
+        internal void CalculateTotalAmount()
+        {
+            Amount = Principal * Math.Pow((1 + (Interest / 100.0)), Time);
+            Console.WriteLine("Total Amount = {0}", Amount.ToString("0.00"));
+        }
+        internal void CalculateCI()
+        {
+            CompoundInterest = Amount - Principal;
+            Console.WriteLine("CompoundInterest = {0}", CompoundInterest.ToString("0.00"));
+        }
+    }
+}
