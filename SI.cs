477d096ec49afd7d7e1d3d63b79c19e5518c40d3
Pradip Magadum <pradipmagadum@gmail.com>
2026-01-30 14:01:28 +0530

    SI.cs

---
+++ SI.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOPsMiniProject
+{
+    internal class SI
+    {
+        Double Principal, interest,SimpleInterest;
+        int time;
+        internal SI(double principal,double interest,int Time)
+        {
+            Principal = principal;
+            this.interest = interest;
+            time = Time;
+        }
+        internal void CalculateSI()
+        {
+            SimpleInterest = (Principal * interest * time)/100;
+        }
+        internal void Display()
+        {
+            Console.WriteLine("SimpleInterest is {0}", SimpleInterest);
+        }
+    }
+}
